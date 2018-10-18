bea9ccda105e6136b3e1330e1d607f8e486b5be9
MaxiPineker <maxi.pineker@gmail.com>
2018-10-18 11:10:57 +0200

    Add comments

--- pointer_fun.c
+++ pointer_fun.c
@@ -4,8 +4,10 @@ void print_integers(int int_value, int* int_pointer);
 void change_integers(int int_value, int* int_pointer);
 
   int main(int argc, char const *argv[]) {
-    int int_value = 187;  //value of the int
-    int* int_pointer = &int_value; //adress
+    int int_value = 187;   //value of the int
+    int* int_pointer = 42;   
+    //we must declare the int_pointer with "&" and an other
+    //variable
     print_integers(int_value, int_pointer);
     change_integers(int_value,  int_pointer);
     print_integers(int_value, int_pointer);
