b4e1f9c0626a68973e17c4797638443880ad638a
MaxiPineker <maxi.pineker@gmail.com>
2018-10-18 11:11:38 +0200

    Finished it

--- pointer_fun.c
+++ pointer_fun.c
@@ -5,9 +5,10 @@ void change_integers(int int_value, int* int_pointer);
 
   int main(int argc, char const *argv[]) {
     int int_value = 187;   //value of the int
-    int* int_pointer = 42;   
-    //we must declare the int_pointer with "&" and an other
-    //variable
+    int* int_pointer = &int_value;
+    /*we must declare the int_pointer with "&" and an other
+    variable because if I have only a number it makes an initialization
+    from an integer without a cast*/
     print_integers(int_value, int_pointer);
     change_integers(int_value,  int_pointer);
     print_integers(int_value, int_pointer);
@@ -19,4 +20,6 @@ void change_integers(int int_value, int* int_pointer);
  void change_integers(int int_value, int* int_pointer){
  int_value = 361;
  *int_pointer = int_value;
+ /*We changed the int value and consequently the int_pointer
+ change too*/
  }
